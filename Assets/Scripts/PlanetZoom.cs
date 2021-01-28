b938ad17daa5e620904b2619d412e9483dd4efca
LIOR-PC\Lior <liorovedd1357@gmail.com>
2021-01-29 01:41:43 +0200

    Space

---
+++ Assets/Scripts/PlanetZoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlanetZoom : MonoBehaviour
+{
+    float zoomIn = 1.0f;
+    float zoomTime = 5f;
+    float waitTime = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<Player>().enabled = false;
+        Camera.main.transform.LookAt(gameObject.transform);
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, zoomIn, zoomTime);
+        StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("Planet");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3f;
+    float smoothTime = 0.3f;
+    Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CameraTrack();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = new Vector2(transform.position.x + deltaX, transform.position.y + deltaY);
+
+        transform.position = newXPos;
+    }
+
+    private void CameraTrack()
+    {
+        Vector3 targetPos = transform.TransformPoint(new Vector3(0, 0, -10));
+        Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
